375735111b80b855dc21dbd06e8bde54dcf0cae2
adi33333333334 <88799486+adi33333333334@users.noreply.github.com>
2024-01-19 14:49:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Decoding function for the Lua 'enc' encoding
-function dec(encodedData) {
-    return encodedData.replace(/....../g, function(x) {
-        return String.fromCharCode(parseInt(x, 2));
-    });
-}
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -17,6 +10,19 @@ app.use(cors());
 // Middleware to parse JSON in the request body with an increased payload size limit (e.g., 10MB)
 app.use(bodyParser.json({ limit: '1000mb' }));
 
+// Decoding function for the Lua 'enc' encoding
+function customBase64Decode(encodedData) {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+
+    return encodedData.replace(/....../g, function(x) {
+        let value = 0;
+        for (let i = 0; i < x.length; i++) {
+            value += (x[i] === '1' ? 1 : 0) * Math.pow(2, 5 - i);
+        }
+        return charset[value];
+    });
+}
+
 // Variable to store the decoded data
 let decodedData = "No Data";
 
@@ -28,8 +34,8 @@ app.all('/api/data', (req, res) => {
 
         // Ensure that the received data is a string
         if (typeof requestData.encryptedData === 'string') {
-            // Decode the data using the 'dec' function
-            decodedData = dec(requestData.encryptedData);
+            // Decode the data using the 'customBase64Decode' function
+            decodedData = customBase64Decode(requestData.encryptedData);
         } else {
             // Invalid data, respond accordingly
             return res.status(400).send('Invalid request data');
